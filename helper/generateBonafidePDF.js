bc41b0bd8aafeab28e7ae30124b287b344658716
Sooriya04 <sooriya.socials@gmail.com>
2025-09-09 20:12:22 +0530

    used wkhtmltopdf

--- helper/generateBonafidePDF.js
+++ helper/generateBonafidePDF.js
@@ -1,26 +1,23 @@
 const ejs = require('ejs');
-const puppeteer = require('puppeteer');
 const path = require('path');
+const wkhtmltopdf = require('wkhtmltopdf');
+const { promisify } = require('util');
+
+const wkhtmltopdfAsync = promisify(wkhtmltopdf);
 
 async function generateBonafidePDF(formData) {
   const templatePath = path.join(__dirname, '../views/bonafideTemplate.ejs');
   const html = await ejs.renderFile(templatePath, { formData });
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  const buffer = await wkhtmltopdfAsync(html, {
+    pageSize: 'A4',
+    marginTop: '20mm',
+    marginRight: '20mm',
+    marginBottom: '20mm',
+    marginLeft: '20mm',
+    printMediaType: true,
   });
 
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0' });
-
-  const buffer = await page.pdf({
-    format: 'A4',
-    printBackground: true,
-    margin: { top: '20mm', right: '20mm', bottom: '20mm', left: '20mm' },
-  });
-
-  await browser.close();
   return buffer;
 }
 
